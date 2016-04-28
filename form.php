05fd87f9f0801554accfc59156aef0905ca2eb50
Diplodocee <ludwa.borra@gmail.com>
2016-04-27 20:03:58 -0500

    Update form.php

--- form.php
+++ form.php
@@ -3,34 +3,23 @@
 		$name = $_POST['name'];
 		$email = $_POST['email'];
 		$message = $_POST['message'];
-		$from = 'Demo Contact Form'; 
+		$subscribe = $_POST['subscribe']
+		$audition = $_POST['audition']
+		$from = $_POST['name']; 
 		$to = 'ludwa.borra@gmail.com'; 
-		$subject = 'Message from Contact Demo ';
+		$subject = 'Message for Deliverance ';
 		
-		$body = "From: $name\n E-Mail: $email\n Message:\n $message";
+		$body = "From: $name\n E-Mail: $email\n Message:\n $message Would you like e-mails about Deliverance events:\n $message Would you like to audition:\n $message";
  
-		// Check if name has been entered
-		if (!$_POST['name']) {
-			$errName = 'Please enter your name';
-		}
-		
-		// Check if email has been entered and is valid
-		if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-			$errEmail = 'Please enter a valid email address';
-		}
-		
-		//Check if message has been entered
-		if (!$_POST['message']) {
-			$errMessage = 'Please enter your message';
-		}
+	
  
 // If there are no errors, send the email
-if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
+
 	if (mail ($to, $subject, $body, $from)) {
-		$result='<div class="alert alert-success">Thank You! I will be in touch</div>';
+		$result='<div class="alert alert-success">Thank You! We will be in touch!</div>';
 	} else {
 		$result='<div class="alert alert-danger">Sorry there was an error sending your message. Please try again later</div>';
 	}
 }
-	}
+	
 ?>
